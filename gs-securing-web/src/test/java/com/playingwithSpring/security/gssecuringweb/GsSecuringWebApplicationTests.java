21c66db8676be97ecd7881ae8f7ba4a014d67a6f
SharonKeane1410 <shazkeane335@gmail.com>
2020-01-29 15:29:58 +0000

    Online tutorial

---
+++ gs-securing-web/src/test/java/com/playingwithSpring/security/gssecuringweb/GsSecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playingwithSpring.security.gssecuringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
